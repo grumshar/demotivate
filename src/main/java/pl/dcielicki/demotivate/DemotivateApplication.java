2ef27548d81bcc9acb24f44565f20a2d1cb0202e
Dawid Cielicki <gamer@o2.pl>
2019-01-22 03:28:26 +0100

    Initial commit

---
+++ src/main/java/pl/dcielicki/demotivate/DemotivateApplication.java
@@ -0,0 +1,14 @@
+package pl.dcielicki.demotivate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotivateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotivateApplication.class, args);
+    }
+
+}
+
